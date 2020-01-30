fcb353f0e56d86faafdb545a079ede741b0e23e8
Adil Sadout <adelsadout@hotmail.com>
2020-01-30 22:55:38 +0100

    deleted commented code

--- code/wifiaccesspoint_webpage_edited_ip/wifiaccesspoint_webpage_edited_ip.ino
+++ code/wifiaccesspoint_webpage_edited_ip/wifiaccesspoint_webpage_edited_ip.ino
@@ -13,7 +13,6 @@
 
 const char index_html[] PROGMEM={"<!DOCTYPE html>\n<html>\n<head>\n\t<title>ESP8266 TEST</title>\n\t<meta http-equiv=\"refresh\" content=\"5\">\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n\t<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" >\n\n\n</head>\n<body>\n\t<div class=\"title\">\n\t\t<h1>Testing The Esp8266</h1>\n\t</div>\n\t<div class=\"container\">\n\t\t<div class=\"temp infobox\">\n\t\t\t<h2>Temperature</h2>\n\t\t\t<p>30 C</p>\n\t\t</div>\n\n\t\t<div class=\"humid infobox\">\n\t\t\t<h2>Humidity</h2>\n\t\t\t<p>46%</p>\n\t\t</div>\n\t</div>\n\n\n\t<script type=\"text/javascript\">\n\t\t//Variables\n\t\tvar humidity= \"25%\"; //the value should be a variable that stores the output of a sensor\n\t\tvar temperature= \"47C\" //the value should be a variable that stores the output of a sensor\n\n\n\t\t//selecting the temperature & humidity\n\n\t\t//temperature\n\t\tvar temp = document.querySelector(\".temp p\");\n\n\n\t\t//humidity\n\t\tvar humid = document.querySelector(\".humid p\");\n\n\n\n\t\t//changing the variable\n\t\ttemp.textContent = temperature;\n\n\t\thumid.textContent = humidity;\n\t</script>\n\n</body>\n</html>"};
 const char style_css[] PROGMEM={"*{\n\tmargin: 0;\n\tpadding: 0;\n\ttext-align: center;\n}\n\n.title{\n\tpadding: 30px;\n}\n\n\n\n\n.container{\n\tpadding: 50px 0;\n\tbackground-color: lightgray;\n}\n\n\n\n.infobox{\n\twidth: 300px;\n\tdisplay: inline-block;\n\tbackground-color: lightblue;\n\tmargin: 0 60px;\n\theight: 250px;\n\tposition: relative;\n}\n\n.infobox h2{\n\tpadding: 15px;\n\tfont-size: 40px;\n}\n\n.infobox p{\n\tfont-size: 70px;\n\tposition: absolute;\n\tleft: 50%;\n\ttop: 50%;\n\ttransform: translate(-50%,-50%);\n}"};
-//const char script_js[] PROGMEM={"//Variables\nvar humidity= \"25%\"; //the value should be a variable that stores the output of a sensor\nvar temperature= \"47C\" //the value should be a variable that stores the output of a sensor\n\n\n//selecting the temperature & humidity\n\n//temperature\nvar temp = document.querySelector(\".temp p\");\n\n\n//humidity\nvar humid = document.querySelector(\".humid p\");\n\n\n\n//changing the variable\ntemp.textContent = temperature;\n\nhumid.textContent = humidity;"};
 
 /* Set these to your desired credentials. */
 
@@ -58,7 +57,6 @@ void setup() {
   IPAddress myIP = WiFi.softAPIP();
   server.on("/", handleRoot);
   server.on("/style.css", handleCss);
-  /*server.on("/script.js", handleJs);*/
   server.begin();
   Serial.println("HTTP server started");
 }
